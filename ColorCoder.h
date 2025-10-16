a9444ef0a695b5208e3c4a98eaf5fb2b38757809
hanishfire16-hue <hanishfire16@gmail.com>
2025-10-16 15:30:22 +0530

    Update ColorCoder.h

--- ColorCoder.h
+++ ColorCoder.h
@@ -1,28 +1,8 @@
-
 #pragma once
-#include <string>
+#include "ColorPair.h"
 
 namespace TelCoColorCoder
 {
-    void PrintColorCodeManual();
-    enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
-    enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
-
-    extern const char* MajorColorNames[];
-    extern const char* MinorColorNames[];
-    extern const int numberOfMajorColors;
-    extern const int numberOfMinorColors;
-
-    class ColorPair {
-        MajorColor majorColor;
-        MinorColor minorColor;
-    public:
-        ColorPair(MajorColor major, MinorColor minor);
-        MajorColor getMajor() const;
-        MinorColor getMinor() const;
-        std::string ToString() const;
-    };
-
     ColorPair GetColorFromPairNumber(int pairNumber);
     int GetPairNumberFromColor(MajorColor major, MinorColor minor);
 }
